e33dfed3f76c11ad06045f6812699a214889f113
abhijithab15 <125530741+abhijithab15@users.noreply.github.com>
2023-11-02 10:43:54 +0530

    Add files via upload

---
+++ Cycle 2/4/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.toogle_image;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private FrameLayout frameLayout;
+    private ImageView imageView1;
+    private ImageView imageView2;
+    private Button toggleButton;
+    private boolean isImage1Visible = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        frameLayout = findViewById(R.id.frameLayout);
+        imageView1 = findViewById(R.id.imageView1);
+        imageView2 = findViewById(R.id.imageView2);
+        toggleButton = findViewById(R.id.toggleButton);
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isImage1Visible) {
+                    imageView1.setVisibility(View.INVISIBLE);
+                    imageView2.setVisibility(View.VISIBLE);
+                } else {
+                    imageView1.setVisibility(View.VISIBLE);
+                    imageView2.setVisibility(View.INVISIBLE);
+                }
+                isImage1Visible = !isImage1Visible;
+            }
+        });
+    }
+}
